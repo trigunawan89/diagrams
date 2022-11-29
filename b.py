0b79b920e1f8b48d72ffbda37a7264a695a047b0
tri gunawan <tri.gunawan@live.com>
2022-11-29 16:58:33 +0700

    remove others

--- b.py
+++
@@ -1,8 +0,0 @@
-from diagrams import Diagram, Cluster
-from diagrams.programming.flowchart import Database, Action
-
-with Diagram("odoo", show=False, direction="TB"):
-    database1 = Database("Postgresql")
-    proses1 = Action("API")
-
-    database1 >> proses1
